2701227dbc6d61eda7896b65ad608358e2d1977c
Gus <58223632+thegu5@users.noreply.github.com>
2025-12-09 15:26:41 +0000

    consolidate tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	outputOptions: {
         banner: `/* @ts-self-types="./index.d.ts" */`,
-    }
+    },
+    fixedExtension: false
 });
