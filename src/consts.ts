e46c41ced8e729aac8d2672b351cc5140c315c16
Gus <58223632+thegu5@users.noreply.github.com>
2026-04-28 11:01:08 -0400

    replace typescript enums with `as const` objects

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,22 @@
-export enum Region {
-	// Unknown = 0,
-	// Oceania = 1,
-	Asia = 2,
-	Europe = 3,
-	NorthAmerica = 4,
-	// SouthAmerica = 5
-}
+export const Region = {
+	// Unknown: 0,
+	// Oceania: 1,
+	Asia: 2,
+	Europe: 3,
+	NorthAmerica: 4,
+	// SouthAmerica: 5
+} as const;
+export type Region = (typeof Region)[keyof typeof Region];
 
-export enum MatchResult {
-	Draw = -1,
-	Loss = 0,
-	Win = 1,
-}
+export const MatchResult = {
+	Draw: -1,
+	Loss: 0,
+	Win: 1,
+} as const;
+export type MatchResult = (typeof MatchResult)[keyof typeof MatchResult];
 
-export enum LeaderboardOrder {
-	Ascending = 0,
-	Descending = 1,
-}
+export const LeaderboardOrder = {
+	Ascending: 0,
+	Descending: 1,
+} as const;
+export type LeaderboardOrder = (typeof LeaderboardOrder)[keyof typeof LeaderboardOrder];
